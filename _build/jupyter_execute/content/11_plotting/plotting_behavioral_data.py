45e5f4f7ca7c99e9084b5cc5f9b7fb8a1b6d9e42
JeshuaT <jeshuatromp@live.nl>
2022-11-10 16:03:26 +0100

    Compiled book with plotting tutorials updated

--- _build/jupyter_execute/content/11_plotting/plotting_behavioral_data.py
+++ _build/jupyter_execute/content/11_plotting/plotting_behavioral_data.py
@@ -11,7 +11,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# import seaborn as sns
+import seaborn as sns
 
 # disable chained assignments
 pd.options.mode.chained_assignment = None
