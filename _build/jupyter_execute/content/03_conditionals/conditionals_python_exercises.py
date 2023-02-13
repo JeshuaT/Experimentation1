cf585c1d4aa36c4c5feb519a0d9c9acf30ff54b0
trompjj <jeshuatromp@live.nl>
2023-02-13 14:22:59 +0100

    Updates conditional exercises

--- _build/jupyter_execute/content/03_conditionals/conditionals_python_exercises.py
+++ _build/jupyter_execute/content/03_conditionals/conditionals_python_exercises.py
@@ -46,12 +46,14 @@
 # 
 # Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
 # 
-#  Score   Grade
-# \>= 0.9     A
-# \>= 0.8     B
-# \>= 0.7     C
-# \>= 0.6     D
-#  < 0.6     F
+# | Score  | Grade |
+# |--------|-------|
+# | >= 0.9 | A     |
+# | >= 0.8 | B     |
+# | >= 0.7 | C     |
+# | >= 0.6 | D     |
+# | < 0.6  | F     |
+# 
 # ```
 # Enter score: 0.95
 # A
